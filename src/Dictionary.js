5f3ca6738dcb1c6a320b4071098d575fdf43c23a
elidelmar <88523575+elidelmar@users.noreply.github.com>
2021-11-18 15:37:33 -0500

    added axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary () {
     let [keyword, setKeyword] = useState(""); 
+    
+    function handleResponse(response);
+    
     function search(event) {
          event.preventDefault();
          alert(`Searching for ${keyword} definition`);
-}
+         let apiUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}';
+        axios.get(apiUrl).then(handleResponse);
+        }
+
+
 function handleKeywordChange(event) {
     setKeyword(event.target.value);
 }
