904530b105cf143aa2148dd1ee885ef989cb5642
elidelmar <88523575+elidelmar@users.noreply.github.com>
2021-11-18 15:13:08 -0500

    dictionary component added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <main>
+          <Dictionary /> 
+        </main>
+        <footer className="App-footer"><small> Coded by Elisabeth Marquez</small>
+        </footer>
+      </div>
     </div>
   );
 }
