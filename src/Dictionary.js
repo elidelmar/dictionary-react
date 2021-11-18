6bf6a4a4f5d94b70748bdda1c448d2a52d27e03b
elidelmar <88523575+elidelmar@users.noreply.github.com>
2021-11-18 15:16:12 -0500

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,7 +5,7 @@ export default function Dictionary () {
     let [keyword, setKeyword] = useState(""); 
     function search(event) {
          event.preventDefault();
-        alert('Searching ${keyword}');
+         alert(`Searching for ${keyword} definition`);
 }
 function handleKeywordChange(event) {
     setKeyword(event.target.value);
