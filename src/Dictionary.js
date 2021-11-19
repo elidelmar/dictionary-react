034df39b1648efb4171bc7153f6265183c1bfce7
elidelmar <88523575+elidelmar@users.noreply.github.com>
2021-11-19 16:48:07 -0500

    addition of results and meaning component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,28 +1,34 @@
 import React, { useState } from "react";
-import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
+import "./Dictionary.css";
 
-export default function Dictionary () {
-    let [keyword, setKeyword] = useState(""); 
-    
-    function handleResponse(response);
-    
-    function search(event) {
-         event.preventDefault();
-         alert(`Searching for ${keyword} definition`);
-         let apiUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}';
-        axios.get(apiUrl).then(handleResponse);
-        }
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
 
-function handleKeywordChange(event) {
+  function search(event) {
+    event.preventDefault();
+
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
     setKeyword(event.target.value);
-}
-   return (
-   <div className="Dictionary">
-<form onSubmit={search}>
-    <input type="search" onChange={handleKeywordChange} />
-</form>
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+      <Results results={results} />
     </div>
-    );
-    }
+  );
+}
