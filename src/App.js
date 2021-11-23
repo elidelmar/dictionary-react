44045b7f3b7c844c855a0ebc00081b66f4d55788
elidelmar <88523575+elidelmar@users.noreply.github.com>
2021-11-22 19:41:37 -0500

    font commit added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import Dictionary from "./Dictionary";
+import Font from "./Font";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>English Dictionary</h1>
+        <Font />
         <main>
           <Dictionary /> 
         </main>
-        <footer className="App-footer"><small> Coded by Elisabeth Marquez</small>
+        <footer className="App-footer"><small> Openly Sourced Code by Elisabeth Marquez </small>
         </footer>
       </div>
     </div>
---
+++ src/Font.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Font.css";
+
+export default function Font() {
+  return <h2 className="Font">Hello, please type in a word to begin.</h2>;
+}
