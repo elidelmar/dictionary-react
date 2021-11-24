f2818f778f42fe3240a57eca7a606f495e6bab4f
elidelmar <88523575+elidelmar@users.noreply.github.com>
2021-11-24 15:35:02 -0500

    added phonetic component with sound icon

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,6 +1,18 @@
 import React from "react";
+import "./Phonetic.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeUp } from "@fortawesome/free-solid-svg-icons";
+
 
 export default function Phonetic(props) {
-    console.log(props.phonetic);
-    return "Hello from Phonetic";
+  console.log(props.phonetic);
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="_blank">
+        <FontAwesomeIcon icon={faVolumeUp}></FontAwesomeIcon>
+      </a>
+      <br />
+      {props.phonetic.text}
+    </div>
+  );
 }
